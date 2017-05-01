697324eb87c1a3f154cc9274a7ca241cd92a06e8
Rechunk <gabri.marc@gmx.net>
2017-05-01 19:14:57 +0200

    Calculates extremes

--- lib/calculateExtremes.dart
+++ lib/calculateExtremes.dart
@@ -1,13 +1,27 @@
 import "calculateRoots.dart";
 
-List<double> calculateExtremes(String function, String firstDerivation){
-  List<double> extremes = [];
+List<List<List<num>>> calculateExtremes(String function, String firstDerivation){
 
-  List<int> roots = calculateRoots(firstDerivation);
+  List<List<num>> minimums = [];
+  List<List<num>> maximums = [];
+  List<List<num>> turningPoints = [];
+  List<num> roots = calculateRoots(firstDerivation);
 
-  for (int i = 0; i < roots.length; i++){
-    extremes.add(calculateResultOf(firstDerivation, i));
+  print("The roots of the derivation are $roots");
+  for (int x = 0; x < roots.length; x++){
+    num yBeforeRoot = calculateYOfX(firstDerivation, x-1);
+    num yAfterRoot = calculateYOfX(firstDerivation, x+1);
+
+    print("$yBeforeRoot, $yAfterRoot");
+    if (yBeforeRoot < 0 && yAfterRoot > 0){ // minus to plus = minimum
+      minimums.add([x, calculateYOfX(function, x.toDouble())]);
+    }
+    else if (yBeforeRoot > 0 && yAfterRoot < 0){
+      maximums.add([x, calculateYOfX(function, x.toDouble())]);
+    }
+    else {
+      turningPoints.add([x, calculateYOfX(function, x.toDouble())]);
+    }
   }
-
-  return extremes;
+  return [[minimums], [maximums], [turningPoints]];
 }
