5a7ed6ff0ff5b084b4613c5c3744ff07e6c38a61
Rechunk <gabri.marc@gmx.net>
2017-05-06 15:21:03 +0200

    The input widget you see when launching

---
+++ lib/frontPage.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import "calculateRoots.dart";
+import "calculateExtremes.dart";
+import "main.dart";
+
+class InputWidget extends StatefulWidget {
+  @override
+  InputWidgetState createState() => new InputWidgetState();
+}
+
+class InputWidgetState extends State<InputWidget> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return new Column(
+        children: [
+          new Text("Funktion hier eingeben:", style: new TextStyle(fontSize: 20.0, color: Colors.blue, fontFamily: "Raleway")),
+          new TextField(
+            controller: controller,
+            textAlign: TextAlign.center,
+            style: new TextStyle(fontSize: 20.0, color: Colors.blue),
+          ),
+          new IconButton(
+            icon: new Icon(Icons.search),
+            iconSize: 40.0,
+            color: new Color.fromARGB(255, 88, 88, 88),
+            onPressed: () {
+              populateDerivations();
+              roots = calculateRoots(function).toString();
+              extremes = calculateExtremes(function, derivations[0]);
+
+              navigateToResults(context);
+            },
+          )
+        ]
+    );
+  }
+}
